e28676517fd2a75cf85ac38bbb863660ca3484c6
Your Name <you@example.com>
2017-01-28 14:29:51 +0100

    Updated the db location.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     try {
         // create an instance of our web server
-        MyServer our_web_server("C:\\Users\\VF\\ClionProjects\\server\\server.db");
+        MyServer our_web_server("../server.db");
 
         // make it listen on port 5000
         our_web_server.set_listening_port(5000);
