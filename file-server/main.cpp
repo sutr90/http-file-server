aed6a54131cbe3750c328c8954527ad6f13e3ead
Your Name <you@example.com>
2017-01-28 14:53:47 +0100

    Updated cmake libraries.

--- file-server/main.cpp
+++ file-server/main.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 int main() {
     try {
-        // create an instance of our web server
-        MyServer our_web_server("../server.db");
+        MyServer our_web_server("../../server.db");
 
-        // make it listen on port 5000
         our_web_server.set_listening_port(5000);
-        // Tell the server to begin accepting connections.
         our_web_server.start_async();
 
         cout << "Press enter to end this program" << endl;
