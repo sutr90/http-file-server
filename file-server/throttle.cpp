4e110e487bb6ae4283bb06760071eac3fe3c455b
sutr90 <you@example.com>
2017-02-01 20:22:48 +0100

    Removed speed limit.

--- file-server/throttle.cpp
+++ file-server/throttle.cpp
@@ -8,16 +8,16 @@ void throttle::sleep() {
 }
 
 void throttle::set_sleep_time() {
-    speed_limit = speed_limit * 1024 * 1024;
-    float speed_bytes = speed_limit / 8.0f; // speed limit in bytes
-    float chunk_count = speed_bytes / chunk_size; // number of chunks per second
-    sleep_time = (dlib::uint32) (1000 / chunk_count);
+    if (sleep_time > 0) {
+        speed_limit = speed_limit * 1024 * 1024;
+        float speed_bytes = speed_limit / 8.0f; // speed limit in bytes
+        float chunk_count = speed_bytes / chunk_size; // number of chunks per second
+        sleep_time = (dlib::uint32) (1000 / chunk_count);
+    }
 }
 
-const float throttle::MAX_SPEED = 10.f;
-
 throttle::throttle(float speed_limit_mbit, dlib::uint32 chunk_size_byte) :
-        speed_limit(std::max(speed_limit_mbit, MAX_SPEED)),
+        speed_limit(speed_limit_mbit),
         chunk_size(chunk_size_byte) {
     set_sleep_time();
 }
