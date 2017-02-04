9384e72242ee2675c144715d6edd1b5b9aa8262f
sutr90 <sutr90@gmail.com>
2017-02-04 11:04:52 +0100

    Updated includes.

--- zip-stream/zip_stream.h
+++ zip-stream/zip_stream.h
@@ -1,15 +1,13 @@
 #ifndef PROJECT_ZIP_STREAM_H
 #define PROJECT_ZIP_STREAM_H
 
+#include <iostream>
 #include <dlib/dir_nav.h>
 
 void stream_dirfile(dlib::directory &obj, std::ostream &ostream);
 void stream_dirfile(dlib::file &obj, std::ostream &ostream);
 
-#include <iostream>
-#include <c++/5.3.0/iosfwd>
-
-template<class DIRFILE>
+template<typename DIRFILE>
 void stream_to_zip(DIRFILE &obj, std::ostream &ostream){
     stream_dirfile(obj, ostream);
 }
