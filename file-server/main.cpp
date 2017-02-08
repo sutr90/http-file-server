e22cf45875d447d6e88dd8d4d2821e00a306436b
fricv <fricv@kiv.zcu.cz>
2017-02-08 17:02:16 +0100

    Added better handling of killing the server.

--- file-server/main.cpp
+++ file-server/main.cpp
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include "MyServer.h"
 #include "../logging.h"
 
@@ -6,7 +7,19 @@ using namespace std;
 
 const dlib::logger logan("L.main");
 
+void my_handler(int s){
+    logan << LINFO << "Caught signal " << s;
+
+}
+
 int main() {
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
     try {
         setup_logging();
         config_reader cr("config");
@@ -17,7 +30,8 @@ int main() {
 
         logan << LINFO << "Starting server on port " << config.port;
         file_server.set_listening_port(config.port);
-        file_server.start();
+        file_server.start_async();
+        pause();
     }
     catch (exception &e) {
         logan << LERROR << "Error " << e.what();
